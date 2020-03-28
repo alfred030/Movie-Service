c55861f56dedc059d61a06b438ad611eff4206f6
Steve <stevetalek24@gmail.com>
2020-03-27 20:36:51 -0500

    shopping

---
+++ Shopping TTD/src/test/java/CartTest.java
@@ -0,0 +1,17 @@
+import Shopping.When;
+
+public class CartTest {
+
+//    Given that I visit the site, when I begin shopping, then I expect my cart to be empty.
+
+//    Given I have an empty cart, when I add an Item, then I expect to getTotalPrice() reflect the sum of all the Items in my cart, times the quantities of each item.
+
+//    Given I have an empty cart, when I add more than one of an item, then I expect itemQuantities() to show the number of items I have added.
+
+//    Given I have an empty cart, when I add items, then I expect itemizedList() reflect the items I have added along with their price and quantity.
+//   Given I have an empty cart, when I add more than one of an item, then I expect getTotalPrice() to reflect both the item price and quantity.
+
+//    Given I have a cart with items that are not on sale, when I add items that are on sale, I expect onSaleItems() to include only the items on sale.
+//    code is complete, push it to a new GitHub repository and paste a link to your repository in the space provided.
+
+}
