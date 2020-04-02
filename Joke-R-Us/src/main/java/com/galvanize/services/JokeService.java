70a07cafc9f87ae84a0a5c9eb2c965e036867ade
Steve <stevetalek24@gmail.com>
2020-04-01 19:14:11 -0500

    Joke services

--- Joke-R-Us/src/main/java/com/galvanize/services/JokeService.java
+++ Joke-R-Us/src/main/java/com/galvanize/services/JokeService.java
@@ -10,5 +10,24 @@ public class JokeService {
     }
 
     public List<Joke> getAllJokes() {
+        return jokeRepository.fillAll();
     }
+    
+    public List<Joke> findJoKesByCategory(Category category) { retun jokeRepository.findAllByCategory(category);}
+    
+    public Joke getRandomJoke() { return jokeRepository.findRandomJoke(); }
+    
+    public Joke getRandomJokeByCategory(Category category){
+        String catString = category == Category.NA ? "" : category.toString();
+        return jokeRepository.findRandomJokeByCategory(catString);
+    }
+    
+    public void deleteById(long Id) {
+        JokeService jokeRepository;
+        if(jokeRepository.existById(id)) jokeRepository.deleteById(id);
+        else{
+            throw new RecordNotFoundException("Joke number"+id+" was not found");
+        }
+    }
+    
 }
